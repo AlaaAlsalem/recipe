0b7f54aced4d9f297e9145bdb8b22666d2ef1a8c
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-24 23:43:16 +0200

    Add integration tests

---
+++ spec/features/foods_integration_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Foods", type: :request do
+  describe "GET /foods" do
+    it "returns http success" do
+      get foods_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /foods/:id" do
+    it "returns http success" do
+      food = FactoryBot.create(:food)
+      get food_path(food)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /foods/new" do
+    it "returns http success" do
+      get new_food_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /foods" do
+    it "creates a new food" do
+      expect {
+        post foods_path, params: { food: FactoryBot.attributes_for(:food) }
+      }.to change(Food, :count).by(1)
+      expect(response).to redirect_to(foods_url)
+    end
+  end
+
+  describe "GET /foods/:id/edit" do
+    it "returns http success" do
+      food = FactoryBot.create(:food)
+      get edit_food_path(food)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH /foods/:id" do
+    it "updates the food" do
+      food = FactoryBot.create(:food)
+      patch food_path(food), params: { food: { name: "Updated Name" } }
+      expect(food.reload.name).to eq("Updated Name")
+      expect(response).to redirect_to(food)
+    end
+  end
+
+  describe "DELETE /foods/:id" do
+    it "destroys the food" do
+      food = FactoryBot.create(:food)
+      expect {
+        delete food_path(food)
+      }.to change(Food, :count).by(-1)
+      expect(response).to redirect_to(foods_url)
+    end
+  end
+end
