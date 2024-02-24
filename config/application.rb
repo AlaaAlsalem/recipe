dff1fadfe090fadbc90b276674dfb0b8ab5a03c8
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-24 16:14:18 +0200

    Add nav Bar

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,13 @@ module Recipe
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+# Add the asset stylesheets directory to the asset pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
+
 
     # Configuration for the application, engines, and railties goes here.
     #
