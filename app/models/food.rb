32abb1c544264325b866a521319dd09472dba03e
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-24 11:39:58 +0200

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-    belongs_to :user
-    has_many :foods, through: :culinary_recipe_foods
+  belongs_to :user
+  has_many :foods, through: :culinary_recipe_foods
 end
